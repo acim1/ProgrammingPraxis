cd0cf15eeb1d802f960850d6b8b334fffaded3e1
acim1 <mkeller02@gmail.com>
2014-11-27 14:30:08 -0600

    Did AnArrayOfZeroes program.

---
+++ src/AnArrayOfZeroes.hs
@@ -0,0 +1,34 @@
+module AnArrrayOfZeroes where
+
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.IArray
+import Data.STRef
+
+aoz :: [Int] -> Array Int Int
+aoz [] = listArray (0,0) []
+aoz xs = runSTArray $ do
+    right <- newSTRef end 
+    arr <- newListArray (0,end) xs
+    forM_ [0..end] $ \l -> do
+        r <- readSTRef right
+        when (l < r) $ do
+            x <- readArray arr l
+            y <- readArray arr r
+            f arr right l r x y 
+    return arr
+  where
+    end = (length xs) - 1
+    decr = subtract 1
+    f arr right l r 0 0 = do
+        when (l < r) $ do
+            modifySTRef right (decr)
+            r' <- readSTRef right
+            y  <- readArray arr r'
+            f arr right l r' 0 y
+    f arr right l r 0 y = do
+        writeArray arr l y
+        writeArray arr r 0
+        modifySTRef right (decr) 
+    f arr right l r _ _ = return ()  
