fdf55b6857ad18a83fc59b10352d9ea878616f20
acim1 <mkeller02@gmail.com>
2014-12-26 12:01:34 -0600

    Continuing http://programmingpraxis.com/2014/12/19/diana-cryptosystem/

--- src/DianaCryptosystem.hs
+++ src/DianaCryptosystem.hs
@@ -1,5 +1,6 @@
 module DianaCryptosystem where
 
+import Control.Applicative
 import Data.List
 import Data.Map.Strict (Map,fromList,(!))
 import Data.Maybe (fromJust)
@@ -11,12 +12,30 @@ shift ::  [a] -> Int -> [a]
 shift xs n = let (xs',xs'') = splitAt n xs
              in xs'' ++ xs'
 
+
+onetimepad :: [String]
+onetimepad = ["WHTVI","AUCFU","RETFK","OMSAL",
+              "MYMNE","ZIEGP","UKVTF","WZHOK",
+              "GORWY","WETFR","COYET","OOWHY",
+              "ZPDDA","CMMXT","VYTJI","RRQGU"]             
+
 trigraph :: Map Char [(Char, Char)]
 trigraph = fromList [(ltr, zip abc (shift revabc i)) | (ltr,i) <- zip abc [0..25]]
  where
     revabc = reverse abc
 
-readtg :: Char -> Char -> Char
-readtg r c = fromJust $ lookup c (trigraph ! r)
+readtg :: Char -> Char -> Maybe Char
+readtg r c = lookup c (trigraph ! r)
+
+decipher :: [String] -> Maybe [String]
+decipher xs
+    | length xs < 2 = Nothing
+    | otherwise = let cipher = drop 2 xs
+                      key    = drop 2 <$> find (isPrefixOf xs) (tails onetimepad)
+                  in (take 2 xs ++) <$> go cipher key  
+  where
+    go _  Nothing    = Nothing
+    go xs (Just ys)  = undefined     
+
 
  
