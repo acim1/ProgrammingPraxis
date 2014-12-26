edf48b77f92d9e05e22c5339e0b3c75292225c50
acim1 <mkeller02@gmail.com>
2014-12-26 17:54:54 -0600

    Finished http://programmingpraxis.com/2014/12/19/diana-cryptosystem/

--- src/DianaCryptosystem.hs
+++ src/DianaCryptosystem.hs
@@ -1,8 +1,9 @@
 module DianaCryptosystem where
 
 import Control.Applicative
+import Control.Monad
 import Data.List
-import Data.Map.Strict (Map,fromList,(!))
+import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
 
 abc :: String
@@ -19,23 +20,25 @@ onetimepad = ["WHTVI","AUCFU","RETFK","OMSAL",
               "GORWY","WETFR","COYET","OOWHY",
               "ZPDDA","CMMXT","VYTJI","RRQGU"]             
 
-trigraph :: Map Char [(Char, Char)]
-trigraph = fromList [(ltr, zip abc (shift revabc i)) | (ltr,i) <- zip abc [0..25]]
+trigraph :: M.Map Char [(Char, Char)]
+trigraph = M.fromList [(ltr, zip abc (shift revabc i)) | (ltr,i) <- zip abc [0..25]]
  where
     revabc = reverse abc
 
 readtg :: Char -> Char -> Maybe Char
-readtg r c = lookup c (trigraph ! r)
+readtg r c = lookup c =<< M.lookup r trigraph
 
 decipher :: [String] -> Maybe [String]
 decipher xs
     | length xs < 2 = Nothing
-    | otherwise = let cipher = drop 2 xs
-                      key    = drop 2 <$> find (isPrefixOf xs) (tails onetimepad)
-                  in (take 2 xs ++) <$> go cipher key  
+    | otherwise = let prefix = take 2 xs 
+                      cipher = drop 2 xs
+                      key    = drop 2 <$> find (isPrefixOf prefix) (tails onetimepad)
+                  in (prefix ++) <$> go cipher key  
   where
     go _  Nothing    = Nothing
-    go xs (Just ys)  = undefined     
+    go xs (Just ys)  = zipWithM f xs ys
+    f x y            = zipWithM readtg x y     
 
-
- 
+encipher :: [String] -> Maybe [String]
+encipher = decipher 
