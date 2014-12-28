587f3926c0cb4e0ac344d64218f306fcee364888
acim1 <mkeller02@gmail.com>
2014-12-28 15:23:45 -0600

    Continuing http://programmingpraxis.com/2014/12/12/magic-squares/

--- src/MagicSquares.hs
+++ src/MagicSquares.hs
@@ -1,16 +1,16 @@
 module MagicSquares where
 
 import Control.Monad.ST
-import Data.Array
 import Data.Array.ST
+import Data.Array.Unboxed
 
 data Start  = Top | Bot | Lft | Rht
 data Move   = U | D | L | R
 type Rule   = [[Move]]
 type Order  = Int
 type Cell   = (Int,Int)
-type Arr    = Array (Int,Int) Int
-type STArr s = ST s (STArray s (Int, Int) Int)
+type Arr    = UArray (Int,Int) Int
+type STArr s = ST s (STUArray s (Int, Int) Int)
 
 findStart :: Order -> Start -> (Int,Int)
 findStart n pos = case pos of
@@ -21,7 +21,7 @@ findStart n pos = case pos of
   where center = n `quot` 2
 
 magicSquares :: Order -> Start -> Rule -> Arr
-magicSquares n start rule = runSTArray $ do
+magicSquares n start rule = runSTUArray $ do
     let a    = newArray ((1,1),(n,n)) 0 :: STArr s
     let cell = findStart n start
     ms a cell rule
