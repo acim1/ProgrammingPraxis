4ee4ff5de83894edf2cc2968c481691dd77d0154
acim1 <mkeller02@gmail.com>
2014-12-29 10:20:12 -0600

    Continuing http://programmingpraxis.com/2014/12/12/magic-squares/

--- src/MagicSquares.hs
+++ src/MagicSquares.hs
@@ -1,5 +1,6 @@
 module MagicSquares where
 
+import Control.Monad
 import Control.Monad.ST
 import Data.Array.ST
 import Data.Array.Unboxed
@@ -10,7 +11,7 @@ type Rule   = [[Move]]
 type Order  = Int
 type Cell   = (Int,Int)
 type Arr    = UArray (Int,Int) Int
-type STArr s = ST s (STUArray s (Int, Int) Int)
+type STArr s = STUArray s (Int, Int) Int
 
 findStart :: Order -> Start -> (Int,Int)
 findStart n pos = case pos of
@@ -18,13 +19,20 @@ findStart n pos = case pos of
                 Bot -> (n, center)
                 Lft -> (center, 1)
                 Rht -> (center, n)
-  where center = n `quot` 2
+  where 
+    center = n `quot` 2
 
 magicSquares :: Order -> Start -> Rule -> Arr
 magicSquares n start rule = runSTUArray $ do
-    let a    = newArray ((1,1),(n,n)) 0 :: STArr s
+    a <- newArray ((1,1),(n,n)) 0 :: ST s (STArr s)
     let cell = findStart n start
-    ms a cell rule
-    
-ms :: STArr s -> Cell -> Rule -> STArr s
-ms a c r = undefined 
+    ms a n cell rule
+
+-- possibly uneccessary function? do a map of rules over array and be done with it?    
+ms :: STArr s -> Order -> Cell -> Rule -> ST s (STArr s)
+ms a n c r = go 1
+  where 
+    go i
+      | i > n     = return a
+      | otherwise = return a  
+
